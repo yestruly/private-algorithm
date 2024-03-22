b63b13fbc25107d7efbd52dce018f8ceef3ad468
[yestruly] <dpwls143@gmail.com>
2024-03-22 14:37:43 +0900

    프로그래머스 완전탐색

--- src/main/java/프로그래머스Lv2/카펫.java
+++ src/main/java/프로그래머스Lv2/카펫.java
@@ -1,19 +1,14 @@
 package 프로그래머스Lv2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class 카펫 {
-  public static int[] solution(int brown, int yellow) {
+  public int[] solution(int brown, int yellow) {
     int[] answer = new int[2];
     for (int i = 3; i <brown+yellow ; i++) {
       int height = i;
       int width = (brown+yellow)/height;
-
       if(width<3){
         continue;
       }
-      System.out.println(height+" "+width);
 
       if(height<=width){
         if ((height+width-2)*2 == brown && (height-2)*(width-2) == yellow){
@@ -24,9 +19,4 @@ public class 카펫 {
     }
     return answer;
   }
-
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(solution(8,1)));
-  }
-
 }
