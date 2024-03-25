84e7f24359c7248535a288e6710cbfcc5a7a1321
[yestruly] <dpwls143@gmail.com>
2024-03-25 16:42:49 +0900

    프로그래머스 + 백준(부분 합, 슬라이딩 윈도우)

--- src/main/java/프로그래머스Lv2/타겟_넘버.java
+++ src/main/java/프로그래머스Lv2/타겟_넘버.java
@@ -4,24 +4,19 @@ public class 타겟_넘버 {
 
   static int answer;
 
-  public static int solution(int[] numbers, int target) {
+  public int solution(int[] numbers, int target) {
 
     dfs(numbers, 0, target, 0);
     return answer;
   }
 
   static void dfs(int[] numbers, int idx, int target, int sum) {
-    if (numbers.length == idx) {
-      if (sum == target) {
-        answer++;
-      }
-    } else {
-      dfs(numbers, idx+1, target, sum + numbers[idx]);
-      dfs(numbers, idx+1, target, sum - numbers[idx]);
+    if(sum==target){
+      answer++;
+    }else{
+      dfs(numbers, idx+1, target, sum+numbers[idx]);
+      dfs(numbers, idx+1, target, sum-numbers[idx]);
     }
   }
-
-  public static void main(String[] args) {
-    System.out.println(solution(new int[]{1,1,1,1,1},3));
-  }
 }
+
