a8dffa40c3700fe0d36efdc519ad3d54452d97fd
[yestruly] <dpwls143@gmail.com>
2024-04-01 13:49:11 +0900

    프로그래머스 + 백준

--- src/main/java/프로그래머스Lv1/문자열_내_마음대로_정렬하기.java
+++ src/main/java/프로그래머스Lv1/문자열_내_마음대로_정렬하기.java
@@ -1,11 +1,12 @@
 package 프로그래머스Lv1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 
 public class 문자열_내_마음대로_정렬하기 {
-  public String[] solution(String[] strings, int n) {
+  public static String[] solution(String[] strings, int n) {
     String[] answer = new String[strings.length];
     HashMap<String, Integer> map = new HashMap<>();
     for (int i = 0; i < strings.length; i++) {
@@ -21,6 +22,11 @@ public class 문자열_내_마음대로_정렬하기 {
       idx++;
     }
     return answer;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(Arrays.toString(solution(new String[]{"abce", "abcd", "cdx"},2)));
   }
 
 }
--- src/main/java/프로그래머스Lv2/귤_고르기.java
+++ src/main/java/프로그래머스Lv2/귤_고르기.java
@@ -6,29 +6,26 @@ import java.util.HashMap;
 import java.util.List;
 
 public class 귤_고르기 {
-  public static int solution(int k, int[] tangerine) {
+
+  public int solution(int k, int[] tangerine) {
     int answer = 0;
     HashMap<Integer, Integer> map = new HashMap<>();
-    for(int item : tangerine){
-      map.put(item, map.getOrDefault(item,0)+1);
+    for (int item : tangerine) {
+      map.put(item, map.getOrDefault(item, 0) + 1);
     }
 
-    List<Integer>list = new ArrayList<>(map.keySet());
+    List<Integer> list = new ArrayList<>(map.keySet());
     Collections.sort(list,
         (v1, v2) -> (map.get(v2).compareTo(map.get(v1))));
 
-    for(int item : list){
-      int val =k-=map.get(item);
+    for (int item : list) {
+      int val = k -= map.get(item);
       answer++;
-      if(val<=0){
+      if (val <= 0) {
         break;
       }
     }
     return answer;
-  }
-
-  public static void main(String[] args) {
-    System.out.println(solution(6, new int[]{1,3,2,5,4,5,2,3}));
   }
 
 }
